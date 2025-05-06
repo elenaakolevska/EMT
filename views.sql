06e367f7dd25b27231bb3e9330f61302fc1ffb6d
Elena Kolevska <kolevska_elena@yahoo.com>
2025-05-06 13:58:46 +0200

    Additional requirement implemented.

--- views.sql
+++ views.sql
@@ -1,11 +1,11 @@
--- CREATE MATERIALIZED VIEW display_accommodations_by_host AS
--- SELECT
---     h.name,
---     h.surname,
---     COUNT(a.id) AS num_accommodations
--- FROM
---     host h
---         LEFT JOIN
---     accommodation a ON a.host_id = h.id
--- GROUP BY
---     h.name, h.surname;
+CREATE MATERIALIZED VIEW display_accommodations_by_host AS
+SELECT
+    h.name,
+    h.surname,
+    COUNT(a.id) AS num_accommodations
+FROM
+    host h
+        LEFT JOIN
+    accommodation a ON a.host_id = h.id
+GROUP BY
+    h.name, h.surname;
