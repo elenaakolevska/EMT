e030444a7dc3e49059123b98a45d705e82528b50
Elena Kolevska <kolevska_elena@yahoo.com>
2025-05-17 18:55:23 +0200

    Added frontend

---
+++ Backend/src/main/java/mk/ukim/finki/dto/display/DisplayCountryDto.java
@@ -0,0 +1,17 @@
+package mk.ukim.finki.dto.display;
+
+import mk.ukim.finki.model.domain.Country;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record DisplayCountryDto(String name, String continent) {
+    public static DisplayCountryDto from (Country country){
+        return new DisplayCountryDto(
+                country.getName(),
+                country.getContinent());
+    }
+    public static List<DisplayCountryDto> from(List<Country> countries){
+        return countries.stream().map(DisplayCountryDto::from).collect(Collectors.toList());
+    }
+}
